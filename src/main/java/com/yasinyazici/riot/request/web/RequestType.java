69a0f5c9e50284e996c96930cddcebbdb7f111e7
YasinY <yasin_programmer@hotmail.com>
2016-02-23 21:01:37 +0100

    Added some more request types, more to come

--- src/main/java/com/yasinyazici/riot/request/web/RequestType.java
+++ src/main/java/com/yasinyazici/riot/request/web/RequestType.java
@@ -2,13 +2,21 @@ package com.yasinyazici.riot.request.web;
 
 /**
  * Created by Yasin on 10/02/2016.
- * //TODO EDIT SO THAT IT GIVES FULL LINK ~INSTEAD OF MERGING WHY NOT HAVE FULL
+ * //TODO MAKE %% TAGS DECLARE THAT MULTIPLE SUMMONERIDS/SUMMONERNAMES CAN BE GIVEN
  */
 public enum RequestType {
     GET_CHAMPION_LIST("v1.2/champion"),
     GET_CHAMPION_BY_ID("v1.2/champion/%id%"),
     GET_SUMMONER_DATA_BY_NAME("v1.4/summoner/by-name/%name%"),
-    GET_SUMMONER_STATS_BY_ID("v1.3/stats/by-summoner/%summonerId%/summary");
+    GET_SUMMONER_STATS_BY_ID("v1.3/stats/by-summoner/%summonerId%/summary"),
+    GET_SUMMONER_MASTERIES_BY_ID("v1.4/summoner/%summonerId%/masteries"),
+    GET_SUMMONER_RUNES_BY_ID("v1.4/summoner/%summonerIds%/runes"),
+    GET_SUMMONER_NAMES_BY_ID("v1.4/summoner/%summonerIds%/name"),
+    GET_RANKED_STATS_BY_SUMMONER_ID("v1.3/stats/by-summoner/%summonerId%/ranked"),
+    GET_SUMMARY_STATS_BY_SUMMONER_ID("v1.3/stats/by-summoner/%summonerId%/summary"),
+    GET_MATCHLIST_BY_SUMMONER_ID("v2.2/matchlist/by-summoner/%summonerId%"),
+    GET_MATCH_BY_ID("v2.2/match/%matchId%"),
+    GET_RECENT_GAME_BY_SUMMONER_ID("v1.3/game/by-summoner/%summonerId%/recent");
 
     private String link;
 
