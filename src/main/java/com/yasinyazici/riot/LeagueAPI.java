37ebed21fa4be6dc0fbb295ed0739501df047285
Yasin Yazici <yasin_programmer@hotmail.com>
2016-12-19 18:27:26 +0100

    Oops, getPlatformId will be nowhere else used but in the LeagueApi class, reversed a change

--- src/main/java/com/yasinyazici/riot/LeagueAPI.java
+++ src/main/java/com/yasinyazici/riot/LeagueAPI.java
@@ -1,6 +1,5 @@
 package com.yasinyazici.riot;
 
-import com.yasinyazici.riot.config.Config;
 import com.yasinyazici.riot.config.json.impl.*;
 import com.yasinyazici.riot.data.activegame.CurrentGameInfo;
 import com.yasinyazici.riot.data.champion.ChampionInfo;
@@ -32,6 +31,12 @@ public class LeagueAPI {
         requestCreator.getRequestProperty().setParameters(region);
         return new GameVersionParser(requestCreator.create().getResponseMessage()).get()[0];
     }
+
+    synchronized ChampionMastery getChampionMastery(String region, long summonerId, long championId) throws Exception {
+        requestCreator.getRequestProperty().setRequestType(RegionalRequestType.GET_CHAMPION_MASTERY);
+        requestCreator.getRequestProperty().setParameters(region, getPlatformId(region), summonerId, championId);
+        return new ChampionMasteryParser(requestCreator.create().getResponseMessage()).get();
+    }
     synchronized Summoner getSummoner(String region, String summonerName) throws Exception {
         requestCreator.getRequestProperty().setRequestType(ApiRequestType.GET_SUMMONER_DATA_BY_NAME);
         requestCreator.getRequestProperty().setParameters(region, summonerName);
@@ -45,8 +50,37 @@ public class LeagueAPI {
     }
     synchronized CurrentGameInfo getActiveGame(String region, long summonerId) throws Exception {
         requestCreator.getRequestProperty().setRequestType(RegionalRequestType.GET_CURRENT_GAME);
-        requestCreator.getRequestProperty().setParameters(region, Config.getPlatformId(region), summonerId);
+        requestCreator.getRequestProperty().setParameters(region, getPlatformId(region), summonerId);
         return new CurrentGameInfoParser(requestCreator.create().getResponseMessage()).get();
     }
 
+
+    public synchronized String getPlatformId(String region) throws PropertyNotFound {
+        switch (region) {
+            case "na":
+                return "NA1";
+            case "euw":
+                return "EUW1";
+            case "eune":
+                return "EUN1";
+            case "jp":
+                return "JP1";
+            case "kr":
+                return "KR";
+            case "oce":
+                return "OC1";
+            case "br":
+                return "BR1";
+            case "lan":
+                return "LA1";
+            case "las":
+                return "LA2";
+            case "ru":
+                return "RU";
+            case "tr":
+                return "TR1";
+            default:
+                throw new PropertyNotFound("Property not found");
+        }
+    }
 }
