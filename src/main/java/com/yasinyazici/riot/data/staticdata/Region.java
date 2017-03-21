6135f1caeea062b13aa9d206a49e3e9cecf2cde8
Yasin Yazici <yasin_programmer@hotmail.com>
2017-03-21 14:34:16 +0100

    Added region identification by string, added some documentation to regions

--- src/main/java/com/yasinyazici/riot/data/staticdata/Region.java
+++ src/main/java/com/yasinyazici/riot/data/staticdata/Region.java
@@ -1,7 +1,10 @@
 package com.yasinyazici.riot.data.staticdata;
 
+import java.util.stream.Stream;
+
 /**
- * Created by Yasin on 08.02.2017
+ * <p>This class is responsible for the identification of the location in which requests are being made</p>
+ * @author Yasin on 08.02.2017
  * E-mail: yasin_programmer@hotmail.com
  * Github: YasinY
  */
@@ -16,20 +19,42 @@ public enum Region {
     OCE("OC1"),
     LAS("LA2"),
     LAN("LA1"),
-    BR("BR1");
+    BR("BR1"),
+    NONE("NONE");
 
     private String platformId;
 
+    /**
+     * Creates a new {@link Region} instance
+     * @param platformId the platform id to assign a region to
+     */
     Region(String platformId) {
         this.platformId = platformId;
     }
 
+    /**
+     * Gets the name of the enum as it equals the region shortcode which also is liable for all kind of requests
+     * @return the name of the enum in lowercase as shortcode
+     */
     public String getShortCode() {
         return this.name().toLowerCase();
     }
 
+    /**
+     * Gets the platform id corresponding to the region for requests like spectator mode
+     * @return platformId the platformId corresponding to a region
+     */
     public String getPlatformId() {
         return platformId;
+    }
+
+    /**
+     * Parses the given String to identify the corresponding region
+     * @param regionCode the region code to identify and return an object on
+     * @return the identified region or {@link Region#NONE} if the string could not get identified
+     */
+    public static Region parseRegion(String regionCode) {
+        return Stream.of(values()).filter(region -> region.getShortCode().equalsIgnoreCase(regionCode)).findFirst().orElse(NONE);
     }
 
 }
